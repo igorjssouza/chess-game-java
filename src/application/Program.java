910919ddac826cbf9c768f2cac469ea2748f7d49
igorjssouza <igorjs.souza@gmail.com>
2023-07-15 15:11:33 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+public class Program {
+
+	public Program() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello wolrd!");
+
+	}
+
+}
